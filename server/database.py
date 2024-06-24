db06bbd77b6ba57a2c18b896e6b5b064233a74b5
FrankSiret <frank.siret@gmail.com>
2024-06-25 00:08:08 +0200

    create new db for auth

--- server/database.py
+++ server/database.py
@@ -7,8 +7,9 @@ db_host = os.environ.get('MYSQL_HOST')
 db_port = os.environ.get('MYSQL_PORT')
 db_username = os.environ.get('MYSQL_USER')
 db_password = os.environ.get('MYSQL_PASSWORD')
+db_database = os.environ.get('MYSQL_DATABASE')
 
-DATABASE_URL = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/auth_db"
+DATABASE_URL = f"mysql+mysqlconnector://{db_username}:{db_password}@{db_host}:{db_port}/{db_database}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
